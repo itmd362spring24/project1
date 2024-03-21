52ee5a36d8b21f42242a207e186416e4d5990656
vburha <142959794+vburha@users.noreply.github.com>
2024-03-21 14:30:08 -0500

    Commenting in style ideas as notes, fixing js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-var submitButton = document.querySelectorAll("h1");
-
-submitButton.onclick = function(){alert("Test successful")};
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
